ff70f85029d576245d7a60f768a1461145741e20
army711 <armandocavallo@gmail.com>
2023-05-08 15:11:33 +0200

    primi test.

--- AirbossLincoln.lua
+++ AirbossLincoln.lua
@@ -9,20 +9,20 @@ local airbossLincoln=AIRBOSS:New("Lincoln", "Lincoln Supercarrier")
 
 -- Opzioni Carrier --
 airbossLincoln=AIRBOSS:SetTrapSheet("C:\\temp\\AirbossStats\\")
-airbossLincoln=AIRBOSS:Load("C:\\temp\\AirbossStats\\")
+-- airbossLincoln=AIRBOSS:Load("C:\\temp\\AirbossStats\\")
 airbossLincoln=AIRBOSS:SetTACAN(74,X, "LIN") -- Set TACAN
+airbossLincoln=AIRBOSS:SetRadio(274)
 airbossLincoln=AIRBOSS:SetICLS(2,"LIN") -- Set ICLS
 airbossLincoln=AIRBOSS:SetPatrolAdInfinitum(true) -- Alla fine del Patrol la carrier Ricomincia
-airbossLincoln=AIRBOSS:SetCarrierControlledArea(60) -- Zona controllata dalla Carrier
+-- airbossLincoln=AIRBOSS:SetCarrierControlledArea(60) -- Zona controllata dalla Carrier
 airbossLincoln=AIRBOSS:SetRecoveryCase(1) -- Recovery Case Default 1
 airbossLincoln=AIRBOSS:AddRecoveryWindow("8:30", "20:00", 1, 15, true, 20)
 airbossLincoln=AIRBOSS:SetCollisionDistance(5)
-airbossLincoln=AIRBOSS:Save("C:\\temp\\AirbossStats\\")
+-- airbossLincoln=AIRBOSS:Save("C:\\temp\\AirbossStats\\")
 airbossLincoln=AIRBOSS:SetAutoSave("C:\\temp\\AirbossStats\\")
-airbossLincoln=AIRBOSS:SetSoundfilesFolder("Airboss Soundfiles/")
-airbossLincoln=AIRBOSS:SetVoiceOversLSOByRaynor()
-airbossLincoln=AIRBOSS:SetVoiceOversMarshalByRaynor()
-airbossLincoln=AIRBOSS:SetUserSoundRadio()
+--airbossLincoln=AIRBOSS:SetSoundfilesFolder("Airboss Soundfiles/")
+--airbossLincoln=AIRBOSS:SetVoiceOversLSOByRaynor()
+--airbossLincoln=AIRBOSS:SetVoiceOversMarshalByRaynor()
 -- FINE Opzioni Carrier --
 
 airbossLincoln:Start() -- Avvia AIRBOSS ULTIMA LINEA DOPO LE OPZIONI
